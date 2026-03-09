b76f7d2885671afdf6281f1be1f9a756c51727ca
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-09 18:45:22 +0100

    Añadiendo formulario al archivo Agregar.php

---
+++ views/agregar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Alumno</title>
+</head>
+
+<body>
+    <h1>Agregar Alumno</h1>
+    <form>
+        Nombre:
+        <input type="text" name="nombre" require><br>
+        <button type="submit">Agregar Alumno</button>
+    </form>
+    <br>
+
+    <a href="index.php">Volver a Index</a>
+
+
+
+</body>
+
+</html>
