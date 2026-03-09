b4732d9aaeb6e9d7eed721281a4e5327e4333950
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-09 18:13:30 +0100

    Añadiendo en index.php acciones con el controller

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 
         <?php
         require_once "autoload.php";
-      
+
 
         $gestor = new GestorPDO();
-        $controller =new Controller($gestor);
+        $controller = new Controller($gestor);
 
         //Consultar datos
         $controller->index();
+
+        $accion = $_GET['accion'] ?? 'index.php';
+
+        switch ($accion) {
+            case 'editar':
+               $controller->editar();
+                break;
+            case 'eliminar':
+               $controller->eliminar();
+                break;
+            case 'agregar':
+                $controller->agregar();
+                break;
+
+            default:
+                $controller->index();
+                break;
+        }
         ?>
