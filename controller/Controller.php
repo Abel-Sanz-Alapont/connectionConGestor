cae8caba5e3c89649872e9110ae23428af03f2bf
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-09 18:53:05 +0100

    Terminando meto Agretar en el controller

--- controller/Controller.php
+++ controller/Controller.php
@@ -4,17 +4,26 @@ class Controller
 {
     protected $gestor;
 
-    public function __construct($gestor) {
-        $this->gestor=$gestor;
+    public function __construct($gestor)
+    {
+        $this->gestor = $gestor;
     }
-    public function index(){
-        $arrayPersonas=$this->gestor->listar();//devuelve array de objetos
+    public function index()
+    {
+        $arrayPersonas = $this->gestor->listar(); //devuelve array de objetos
 
         include "views/listar.php";
     }
 
-    public function agregar(){
+    public function agregar()
+    {
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nombre = $_POST['nombre'];
+
+            $persona = new Persona($nombre);
+            $this->gestor->agregar($persona);
+        }
 
 
         include "views/agregar.php";
