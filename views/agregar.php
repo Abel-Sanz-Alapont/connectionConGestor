25ec3e1a31628470e7f8fa81593cec3a686cd0d9
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-09 19:28:17 +0100

    Fallo en el formulario falta metodo POST

--- views/agregar.php
+++ views/agregar.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Alumno</title>
 </head>
 
 <body>
     <h1>Agregar Alumno</h1>
-    <form>
-        Nombre:
+    <form method="POST">
+        Nombre:<br>
         <input type="text" name="nombre" require><br>
+        
         <button type="submit">Agregar Alumno</button>
     </form>
     <br>
