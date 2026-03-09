885495b537caec47328f4bfe0696240032df20fd
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-09 17:50:21 +0100

    Creando funcion index  para listar el array de objetos

--- controller/Controller.php
+++ controller/Controller.php
@@ -7,5 +7,7 @@ class Controller
     public function __construct($gestor) {
         $this->gestor=$gestor;
     }
-
+    public function index(){
+        $personas=$this->gestor->listar();//devuelve array de objetos 
+    }
 }
