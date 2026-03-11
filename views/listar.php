c69d694a13662cd017731c6081c62f6c6bdd71ee
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-11 12:19:43 +0100

    Agregando Formulario para poder editar los nombres de la BD

--- views/listar.php
+++ views/listar.php
@@ -7,27 +7,36 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
-    <body>
-        <?= "<h1>Hello, Welcome DAW Student!</h1>"; ?>
-        <a href="index.php?accion=agregar">Agregar Alumno a la BD</a>
-        <div class="container-fluid">
-            <?= '<table class="table table-striped">'; ?>
-            <?= '<thead>
+<body>
+    <?= "<h1>Hello, Welcome DAW Student!</h1>"; ?>
+    <a href="index.php?accion=agregar">Agregar Alumno a la BD</a>
+    <div class="container-fluid">
+        <?= '<table class="table table-striped">'; ?>
+        <?= '<thead>
                     <tr>
                         <th>id</th>
                         <th>name</th>
                         <th>Opciones</th>
                     </tr>
                 </thead>'; ?>
-            <?php foreach ($arrayPersonas as $persona):?>
-                    <tr>
-                        <td><?=$persona->getId()?></td>
-                        <td><?=$persona->getNombre()?></td>
-                        <td><a href="index.php?accion=eliminar&id=<?=  $persona->getId()?>">Eliminar </td>
-                    <tr>
-                <?php endforeach ?>
-                </table>
-        </div>
-    </body>
+        <?php foreach ($arrayPersonas as $persona): ?>
+            <tr>
+                <td><?= $persona->getId() ?></td>
+                <td><?= $persona->getNombre() ?></td>
+                <td>
+                    <!--Boton Editar-->
+                    <form method="POST" action="index.php?accion=editar" style="display:inline;">
+                        <input type="hidden" name="id" value="<?= $persona->getId() ?>">
+                        Nombre: <input type="text" name="nombre" value="<?= $persona->getNombre() ?>" required>
+                        <button type="submit">Guardar</button>
+                        <!--Boton Eliminar-->
+                        <td><a href="index.php?accion=eliminar&id=<?= $persona->getId() ?>">Eliminar </td>
+                    </form>
+                </td>
+            <tr>
+            <?php endforeach ?>
+            </table>
+    </div>
+</body>
 
 </html>
